c02686e48996d549d118d2cacd3ce997e82c9945
wfmendez <wuillian.f.mendez@gmail.com>
2024-09-26 09:52:15 -0400

    Fixed specifications in csharp-prep Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name?");
+        Console.Write("What is your first name? ");
         string name = Console.ReadLine();
-        Console.WriteLine("What is your last name?");
+        Console.Write("What is your last name? ");
         string surname = Console.ReadLine();
 
         Console.WriteLine($"Your name is {name}, {name} {surname}.");
